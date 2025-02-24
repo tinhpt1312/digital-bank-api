8ef1a02e1741643a106b578dfa9ef141324b1b14
tinhpt1312 <phamthetinhcop1312@gmail.com>
2025-02-24 10:18:10 +0700

    first commit

---
+++ src/main/java/org/tinhpt/digital/DigitalBankApiApplication.java
@@ -0,0 +1,13 @@
+package org.tinhpt.digital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalBankApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tinhpt/digital/DigitalBankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tinhpt.digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalBankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
